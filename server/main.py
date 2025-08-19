f945c0cd2e2a77cf40e831cd4c7e716c98be9bdc
Ido V. Z <programerido@gmail.com>
2025-08-19 23:33:55 +0300

    Add localhost to CORS allowed origins for local development

--- server/main.py
+++ server/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
